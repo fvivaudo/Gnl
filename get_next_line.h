4742cbca0cc65236dc6074e0057ba2c0e0acb257
Florian VIVAUDOU <fvivaudo@e3r10p1.42.fr>
2015-06-30 17:35:09 +0200

    Gnl

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvivaudo <fvivaudo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/12 10:47:29 by fvivaudo          #+#    #+#             */
+/*   Updated: 2014/11/19 10:26:37 by fvivaudo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 10
+
+int		get_next_line(int const fd, char **line);
+
+#endif
